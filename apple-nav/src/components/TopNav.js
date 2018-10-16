61b33deca276133ee91f1ebbde46c867eeca77e9
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2018-10-16 01:44:02 -0700

    header transition

--- apple-nav/src/components/TopNav.js
+++ apple-nav/src/components/TopNav.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-let TopNav = () => {
+const TopNav = () => {
+  console.log(
+    'test',
+    window.location.pathname.includes('/mac') ||
+      window.location.pathname.includes('/iphone') ||
+      window.location.pathname.includes('/tv')
+  )
   return (
-    <header>
+    <header
+      className={
+        window.location.pathname.includes('/mac') ||
+          window.location.pathname.includes('/iphone') ||
+          window.location.pathname.includes('/tv')
+          ? 'header-dark'
+          : 'header-light'
+      }
+    >
       <nav>
         <Link to='/'><i id='apple' className='fab fa-apple' /></Link>
         <Link to='/mac'>Mac</Link>
