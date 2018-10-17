41b2a35a7f6360e3ee82f8568cee5c5ce967148b
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2018-10-16 22:04:07 -0700

    home updated with second image

--- apple-nav/src/components/Home.js
+++ apple-nav/src/components/Home.js
@@ -1,24 +1,39 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Home = props => {
   return (
-    <section className='cta'>
-      <div className='cta-heading-container'>
-        <h1 className='cta-heading-main'>
-          iPhone X<div id='styleS'>s</div>
-        </h1>
-        <h2 className='cta-heading-sub'>Welcome to the big screens.</h2>
-        <div className='cta-links-container'>
-          <NavLink className='cta-link' to='learn-more'>
-            Learn more &gt;
-          </NavLink>
-          <NavLink className='cta-link' to='store/iphone'>Buy &gt;</NavLink>
+    <Fragment>
+      <section className='cta'>
+        <div className='cta-heading-container'>
+          <h1 className='cta-heading-main'>
+            iPhone X<div id='styleS'>s</div>
+          </h1>
+          <h2 className='cta-heading-sub'>Welcome to the big screens.</h2>
+          <div className='cta-links-container'>
+            <NavLink className='cta-link' to='learn-more'>
+              Learn more &gt;
+            </NavLink>
+            <NavLink className='cta-link' to='store/iphone'>Buy &gt;</NavLink>
+          </div>
         </div>
-      </div>
 
-      <div className='cta-image' alt='cta-image' />
-    </section>
+        <div className='cta-image' alt='cta-image' />
+      </section>
+      <section className='middle-content'>
+        <div className='cta-heading-container'>
+          <h1 className='cta-heading-main'>
+            iPhone X<div id='styleS'><small>R</small></div>
+          </h1>
+          <h2 className='cta-heading-sub'>Brilliant. In every way.</h2>
+          <h3 className='cta-heading-sub'>
+            <small className='pre-order'>Pre-order 10.19</small>
+          </h3>
+        </div>
+
+        <div className='middle-image' alt='cta-image' />
+      </section>
+    </Fragment>
   )
 }
 
